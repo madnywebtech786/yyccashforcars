40600bea761229fe1c24ba2382547df35dd7c7eb
madnywebtech786 <madnywebtech@gmail.com>
2026-01-07 19:27:01 -0700

    removed whatsapp

--- src/app/components/WhatsAppButton.js
+++ src/app/components/WhatsAppButton.js
@@ -1,18 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-
+// export default function WhatsAppButton() {
 export default function WhatsAppButton() {
-  return (
-    <Link href={"https://wa.me/+15877009806"}>
-      <button className="fixed bottom-5 right-4 rounded-full bg-[#25D366] p-3 z-50 hover:scale-110 duration-200">
-        <Image
-          src={"/images/icons/WhatsappIcon.svg"}
-          alt="whatsapp"
-          width={35}
-          height={35}
-        />
-      </button>
-    </Link>
-  );
+  return null;
 }
