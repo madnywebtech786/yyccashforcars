29e9a48d3ae860f7df0443c6924d2bd4e6de2041
jawadamin99 <info@jawadamin.com>
2026-01-19 20:49:52 +0500

    LD JSON

--- src/app/layout.js
+++ src/app/layout.js
@@ -82,32 +82,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-       {/* Google Tag Manager */}
-  <Script id="gtm-script" strategy="afterInteractive">
-    {`
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-KK32N6HP');
     `}
-  </Script>
-  {/* End Google Tag Manager */}
+        </Script>
+        {/* End Google Tag Manager */}
 
-  {/* Google Analytics (gtag.js) */}
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-2LCP9SHQQR"
-    strategy="afterInteractive"
-  />
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2LCP9SHQQR"
+          strategy="afterInteractive"
+        />
 
-  <Script id="ga4-script" strategy="afterInteractive">
-    {`
+        <Script id="ga4-script" strategy="afterInteractive">
+          {`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-2LCP9SHQQR');
     `}
-  </Script>
+        </Script>
       </head>
 
       <body
@@ -130,34 +130,56 @@ export default function RootLayout({ children }) {
         <Footer />
 
         {/* JSON-LD LocalBusiness structured data */}
-        <Script
+        <script
           id="localbusiness-schema"
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
-              "@type": "AutomotiveBusiness",
-              name: "Calgary Cash for Cars",
-              url: "https://www.yyccash.com",
-              logo: "https://www.yyccash.com/images/yyc-car-logo.png",
-              image: "https://www.yyccash.com/images/gallery/g7.jpeg",
-              description:
-                "Top-dollar cash for junk, wrecked and used vehicles in Calgary. Free towing, same-day pickup, licensed paperwork, and eco-friendly recycling. Serving Calgary, Airdrie, Cochrane & Okotoks.",
-      address: {
-              "@type": "PostalAddress",
-              streetAddress: "3412 27 St NE",
-              addressLocality: "Calgary",
-              addressRegion: "AB",
-              postalCode: "T1Y 5E2",
-              addressCountry: "Canada",
-            },
-      serviceArea: {
-                "@type": "Place",
-                name: "Calgary, Airdrie, Cochrane, Okotoks",
-              },
-              sameAs: [
-                "https://www.instagram.com/yyccashforcars",
-              ],
+              "@graph": [
+                {
+                  "@type": "AutomotiveBusiness",
+                  "@id": "https://www.yyccash.com/#localbusiness",
+                  name: "YYC Cash for Cars Calgary",
+                  url: "https://www.yyccash.com",
+                  logo: "https://www.yyccash.com/images/yyc-car-logo.png",
+                  image: "https://www.yyccash.com/images/gallery/g7.jpeg",
+                  description:
+                    "Top-dollar cash for junk, wrecked and used vehicles in Calgary. Free towing, same-day pickup, licensed paperwork, and eco-friendly recycling. Serving Calgary, Airdrie, Cochrane & Okotoks.",
+                  address: {
+                    "@type": "PostalAddress",
+                    streetAddress: "3412 27 St NE",
+                    addressLocality: "Calgary",
+                    addressRegion: "AB",
+                    postalCode: "T1Y 5E2",
+                    addressCountry: "Canada"
+                  },
+                  serviceArea: {
+                    "@type": "Place",
+                    name: "Calgary, Airdrie, Cochrane, Okotoks"
+                  },
+                  sameAs: [
+                    "https://www.instagram.com/yyccashforcars"
+                  ]
+                },
+                {
+                  "@type": "WebSite",
+                  "@id": "https://www.yyccash.com/#website",
+                  url: "https://www.yyccash.com",
+                  name: "YYC Cash For Cars",
+                  alternateName: "YYC Cash For Cars Calgary",
+                  publisher: {
+                    "@id": "https://www.yyccash.com/#localbusiness"
+                  },
+                  inLanguage: "en-US",
+                  "potentialAction": {
+                    "@type": "SearchAction",
+                    "target": "https://yyccash.com/?s={search_term_string}",
+                    "query-input": "required name=search_term_string"
+                  }
+
+                }
+              ]
             }),
           }}
         />
