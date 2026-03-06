4b5033d3dbeded999ac9bdd3550d42c7dafceb28
jawadamin99 <info@jawadamin.com>
2026-03-06 05:54:25 +0500

    2 blogs published

--- src/app/sections/About.js
+++ src/app/sections/About.js
@@ -8,7 +8,7 @@ export default function AboutUsSection() {
           <div className="relative">
             <div className="relative h-96 lg:h-[500px] rounded-2xl overflow-hidden shadow-2xl">
               <img
-                src="/images/car-about.webp"
+                src="/images/blog/scrap-car-removal-calgary-yyc.jpg"
                 alt="About Cash 4Cars"
                 className="w-full h-full object-cover"
               />
