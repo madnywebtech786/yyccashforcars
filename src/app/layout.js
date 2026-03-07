a92229ae7266868a785c9e45169878a7e64cc341
jawadamin99 <info@jawadamin.com>
2026-03-07 05:26:17 +0500

    inspectlet

--- src/app/layout.js
+++ src/app/layout.js
@@ -125,6 +125,26 @@ export default function RootLayout({ children }) {
             })();
           `}
         </Script>
+        <Script id="inspectlet" strategy="beforeInteractive">
+          {`
+            (function() {
+              window.__insp = window.__insp || [];
+              __insp.push(['wid', 1411423155]);
+              var ldinsp = function(){
+                if(typeof window.__inspld != "undefined") return;
+                window.__inspld = 1;
+                var insp = document.createElement('script');
+                insp.type = 'text/javascript';
+                insp.async = true;
+                insp.id = "inspsync";
+                insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=1411423155&r=' + Math.floor(new Date().getTime()/3600000);
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(insp, x);
+              };
+              setTimeout(ldinsp, 0);
+            })();
+          `}
+        </Script>
         {/* Google Tag Manager (noscript) */}
         <noscript
           dangerouslySetInnerHTML={{
